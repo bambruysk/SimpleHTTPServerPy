88abdd1358822c068d3d9789a1328bc067c2cbad
bambruysk <bambruysk@gmail.com>
2021-03-24 08:48:20 +0000

    somple tes

--- server.py
+++ server.py
@@ -1,1 +1,18 @@
+import http.server
+import socketserver
 
+PORT = 8000
+
+class MyHTTPHandler( http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        print("Get response")
+        self.send_response(200)
+        self.end_headers()
+
+
+Handler = MyHTTPHandler
+
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
